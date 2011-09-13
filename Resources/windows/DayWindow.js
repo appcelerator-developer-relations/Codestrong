a9cd494d455ee287a12a549bcde9afd30216353c
tonylukasavage <anthony.lukasavage@gmail.com>
2011-09-13 16:17:16 -0400

    cleanup DayWindow

--- Resources/windows/DayWindow.js
+++ Resources/windows/DayWindow.js
@@ -14,91 +14,86 @@
  * You should have received a copy of the GNU General Public License
  * along with CODESTRONG Mobile.  If not, see <http://www.gnu.org/licenses/>.
  */
+(function () {
 
-(function() {
+    Codestrong.ui.createDayWindow = function (tabGroup) {
+        // Base row properties
+        var baseRow = {
+            hasChild: true,
+            color: '#000',
+            backgroundColor: '#fff',
+            font: {
+                fontWeight: 'bold'
+            }
+        };
+        baseRow[Codestrong.ui.backgroundSelectedProperty + 'Color'] = Codestrong.ui.backgroundSelectedColor;
 
-  Codestrong.ui.createDayWindow = function(tabGroup) {
-    // Base row properties
-    var baseRow = {
-    	hasChild: true,
-    	color: '#000',
-    	backgroundColor: '#fff',
-    	font: {
-    		fontWeight: 'bold'	
-    	} 
+        // Creates a TableViewRow using the base row properties
+        var createDayRow = function (params) {
+            return Codestrong.extend(Ti.UI.createTableViewRow(params), baseRow);
+        }
+
+        // Create data for TableView
+        var data = [
+	        createDayRow({
+	            title: 'Sunday, September 18th',
+	            titleShort: 'September 18th',
+	            scheduleListing: false,
+	            url: 'pages/2011-09-18.html'
+	        }), createDayRow({
+	            title: 'Monday, September 19th',
+	            titleShort: 'September 19th',
+	            start_date: '2011-09-19 00:00:00',
+	            end_date: '2011-09-20 00:00:00',
+	            scheduleListing: true
+	        }), createDayRow({
+	            title: 'Tuesday, September 20th',
+	            titleShort: 'September 20th',
+	            start_date: '2011-09-20 00:00:00',
+	            end_date: '2011-09-21 00:00:00',
+	            scheduleListing: true
+	        }), createDayRow({
+	            title: 'Hackathon',
+	            titleShort: 'Hackathon',
+	            scheduleListing: false,
+	            url: 'pages/hackathon.html'
+	        })
+        ];
+
+        // create main day window
+        var dayWindow = Titanium.UI.createWindow({
+            id: 'win1',
+            title: 'Schedule',
+            backgroundColor: '#fff',
+            barColor: '#414444',
+            fullscreen: false
+        });
+        var tableview = Titanium.UI.createTableView({
+            data: data
+        });
+        dayWindow.add(tableview);
+
+        tableview.addEventListener('click', function (e) {
+            if (e.rowData.scheduleListing) {
+                Codestrong.navGroup.open(Codestrong.ui.createSessionsWindow({
+                    titleShort: e.rowData.titleShort,
+                    title: e.rowData.title,
+                    start_date: e.rowData.start_date,
+                    end_date: e.rowData.end_date
+                }), {
+                    animated: true
+                });
+            } else {
+                Codestrong.navGroup.open(Codestrong.ui.createHtmlWindow({
+                    title: e.rowData.titleShort,
+                    url: e.rowData.url
+                }), {
+                    animated: true
+                });
+            }
+
+        });
+
+        return dayWindow;
     };
-    baseRow[Codestrong.ui.backgroundSelectedProperty + 'Color'] = Codestrong.ui.backgroundSelectedColor;
-    
-    // Creates a TableViewRow using the base row properties
-	var createDayRow = function(params) {
-		return Codestrong.extend(Ti.UI.createTableViewRow(params), baseRow);
-	}
-	
-	// Create data for TableView
-	var data = [
-		createDayRow({
-			title:'Sunday, September 18th', 
-	    	titleShort:'September 18th', 
-	    	scheduleListing: false, 
-	    	url: 'pages/2011-09-18.html'
-		})
-		,
-		createDayRow({
-			title:'Monday, September 19th', 
-	    	titleShort:'September 19th', 
-	    	start_date:'2011-09-19 00:00:00', 
-	    	end_date:'2011-09-20 00:00:00', 
-	    	scheduleListing: true
-		})
-		,
-		createDayRow({
-			title:'Tuesday, September 20th', 
-	    	titleShort:'September 20th', 
-	    	start_date:'2011-09-20 00:00:00', 
-	    	end_date:'2011-09-21 00:00:00', 
-	    	scheduleListing: true
-		})
-		,
-		createDayRow({
-			title:'Hackathon', 
-	    	titleShort:'Hackathon', 
-	    	scheduleListing: false, 
-	    	url: 'pages/hackathon.html'
-		})
-	];
-
-    // create main day window
-    var dayWindow = Titanium.UI.createWindow({
-      id: 'win1',
-      title: 'Schedule',
-      backgroundColor: '#fff',
-      barColor: '#414444',
-      fullscreen: false
-    });
-    var tableview = Titanium.UI.createTableView({
-      data: data
-    });
-    dayWindow.add(tableview);
-
-    tableview.addEventListener('click', function(e) {
-        if (e.rowData.scheduleListing) {
-          Codestrong.navGroup.open(Codestrong.ui.createSessionsWindow({
-          	titleShort: e.rowData.titleShort,
-            title: e.rowData.title,
-            start_date: e.rowData.start_date,
-            end_date: e.rowData.end_date
-          }), {animated:true});
-        }
-        else {
-          Codestrong.navGroup.open(Codestrong.ui.createHtmlWindow({
-            title: e.rowData.titleShort,
-            url: e.rowData.url
-          }), {animated:true});
-        }
-      
-    });
-
-    return dayWindow;
-  };
-
 })();
