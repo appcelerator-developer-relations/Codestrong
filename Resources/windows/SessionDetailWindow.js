cb3682bfef8db983cb1518433daa4d89aba2ddc3
tonylukasavage <anthony.lukasavage@gmail.com>
2011-09-13 15:10:43 -0400

    changed to CODESTRONG in text

--- Resources/windows/SessionDetailWindow.js
+++ Resources/windows/SessionDetailWindow.js
@@ -1,18 +1,18 @@
 /**
- * This file is part of DrupalCon Mobile.
+ * This file is part of CODESTRONG Mobile.
  *
- * DrupalCon Mobile is free software: you can redistribute it and/or modify
+ * CODESTRONG Mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DrupalCon Mobile is distributed in the hope that it will be useful,
+ * CODESTRONG Mobile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DrupalCon Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ * along with CODESTRONG Mobile.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function() {
