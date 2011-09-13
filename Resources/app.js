27f05724c7ddd8cebf3906b45aa7443b8b96d82d
tonylukasavage <anthony.lukasavage@gmail.com>
2011-09-13 13:15:12 -0400

    scrapping more unneeded DrupalCon code

--- Resources/app.js
+++ Resources/app.js
@@ -1,37 +1,41 @@
 /**
- * This file is part of DrupalCon Mobile.
+ * This file is part of CODESTRONG Mobile.
  *
- * DrupalCon Mobile is free software: you can redistribute it and/or modify
+ * CODESTRONG Mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DrupalCon Mobile is distributed in the hope that it will be useful,
+ * CODESTRONG Mobile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DrupalCon Mobile.  If not, see <http://www.gnu.org/licenses/>.
+ * along with CODESTRONG Mobile.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Declaring variables to prevent implied global error in jslint
-var Ti, Titanium, Drupal, desc, menu, refresh, logout;
+var Ti, Titanium, Drupal, desc, refresh, logout;
 
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#414444');
 
-// Include the Drupal connection libraries.
+// include the Codestrong libraries
 Ti.include(
 	'cs/cs.js',
-	'cs/datetime.js',
-  'drupal/drupal.js',
-  'drupal/db.js',
-  'drupal/db.insert.js',
-  'drupal/entity.js',
-  'drupal/entity.datastore.js',
-  'drupalcon/drupalcon.js',
-  'drupalcon/entity.js'
+	'cs/datetime.js'
+);
+
+// Include the Drupal connection libraries.
+Ti.include(
+  	'drupal/drupal.js',
+  	'drupal/db.js',
+  	'drupal/db.insert.js',
+  	'drupal/entity.js',
+  	'drupal/entity.datastore.js',
+  	'drupalcon/drupalcon.js',
+  	'drupalcon/entity.js'
 );
 
 // Register our database information.
@@ -41,10 +45,10 @@ Ti.Database.install('main.sql', 'main');
 // If we haven't created the tables yet, make empty ones to ensure that the
 // app doesn't crash.
 if (!Drupal.db.getConnection('main').tableExists('node')) {
-  Drupal.entity.db('main', 'node').initializeSchema();
+  	Drupal.entity.db('main', 'node').initializeSchema();
 }
 if (!Drupal.db.getConnection('main').tableExists('user')) {
-  Drupal.entity.db('main', 'user').initializeSchema();
+  	Drupal.entity.db('main', 'user').initializeSchema();
 }
 
 Ti.include(
@@ -68,5 +72,5 @@ Ti.include(
 
 // open (sponsor) URLs in the native browser, not a webview
 Ti.App.addEventListener('openURL', function(e){
-  Ti.Platform.openURL(e.url);
+  	Ti.Platform.openURL(e.url);
 });
--- Resources/drupalcon/drupalcon.js
+++ Resources/drupalcon/drupalcon.js
@@ -23,31 +23,6 @@ var DrupalCon = {
 
 
 (function() {
-  var verticalPadding = 2;
-  var presenterList = {};
-
-  DrupalCon.util.getPresenterList = function() {
-    if (!Object.keys(presenterList).length) {
-        var rows = Drupal.db.getConnection('main').query('SELECT name, full_name FROM user');
-        while (rows.isValidRow()) {
-          presenterList[rows.fieldByName('name')] = rows.fieldByName('full_name');
-          rows.next();
-        }
-        rows.close();
-      }
-      return presenterList;
-  };
-
-  DrupalCon.util.getPresenterName = function(name) {
-    var list = DrupalCon.util.getPresenterList();
-    return list[name] || '';
-  };
-
-  // Clear the presenter list cache when we update data.
-  Ti.addEventListener('drupal:entity:datastore:update_completed', function(e) {
-    presenterList = {};
-  });
-
 
   var lastTime = '';
 
