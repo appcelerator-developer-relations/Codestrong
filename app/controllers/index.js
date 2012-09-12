09e99b77b63a0b59607f559caf1d6b2452242c60
Kevin Whinnery <kevin.whinnery@gmail.com>
2012-09-12 10:08:54 -0500

    swapped out login behavior with appcelerator network login, now need to investigate other errors

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,74 +1,71 @@
-/**
- * Alloy Hackathon 08/20/2012
- * Author: Fred Spencer
- * Email: fspencer@appceleator.com
- * == NOTES ==
- *     - There is repetitive code
- *       between profile and userDetail.
- *     - Some styles are not optimal.
- *     - Some model and controller methods
- *       should be in a different location.
- *     - GC hasn't been examined.
- *     - Had problems with widgets.
- *     - Not using 'controller events'.
- */
+//Dependencies
+var User = Alloy.getModel('User');
 
-/**
- * index Controller
- * Builds and renders initial login context.
- * TODO: Drastically improve user creation.
- */
- 
-var __login     = Alloy.getModel( 'login' ),
-    __animation = Alloy.getModel( 'animation' );
+//Generic login error - TODO: make these more specific
+function showError() {
+	var d = Ti.UI.createAlertDialog({
+		title:L('loginError'),
+		message:L('loginErrorText')
+	}).show();
+}
 
-Alloy.CFG.currentWin = $.parent;
+//zoom action
+function zoom(view, callback) {
+	var matrix = Ti.UI.create2DMatrix({ scale:1.5 });
+        
+	view.animate({ 
+		transform:matrix, 
+		opacity:0.0, 
+		duration:250 
+	}, function() {
+    	callback();
+	});
+        
+}
 
-$.username.addEventListener('focus', function(e) {
-    __login.onFocus( e, $.parent );
-});
+//Handle field movement (iOS only)
+if (OS_IOS) {
+	function onFocus(e) {
+	    $.parent.animate({ 
+	    	top:"-130dp", 
+	    	duration:250 
+	    });
+	}
+		
+	function onBlur(e) {
+	    $.parent.animate({ 
+	    	top:0, 
+	    	duration:250 
+	    });
+	}
+	
+	$.username.on('focus', onFocus);
+	$.password.on('focus', onFocus);
+	$.username.on('blur', onBlur);
+	$.password.on('blur', onBlur);
+}
 
-$.password.addEventListener('focus', function(e) {
-    __login.onFocus( e, $.parent );
-});
-
-$.username.addEventListener('blur', function(e) {
-    __login.onBlur( e, $.parent );
-});
-$.password.addEventListener('blur', function(e) {
-    __login.onBlur( e, $.parent );
-});
-
-$.parent.addEventListener('click', function(e) {
+$.parent.on('click', function(e) {
     $.username.blur();
     $.password.blur();
 });
 
-$.loginBtn.addEventListener('click', function(e) {
-    
-    __login.login($.username.value, $.password.value, function(e) {
-        
-        __animation.zoom($.parent, function() {
-            Alloy.getController( 'app' );
+$.loginBtn.on('click', function(e) {
+	User.login($.username.value, $.password.value, function() {
+		zoom($.parent, function() {
+            var App = Alloy.createController('app');
+			App.open();
         });
-        
-    });
-    
+	}, function() {
+		showError();
+	});
 });
 
-$.createBtn.addEventListener('click', function(e) {
-    
-    __login.create($.username.value, $.password.value, function(e) {
-        
-        __animation.zoom($.parent, function() {
-            Alloy.getController( 'app' );
-        });
-        
-    });
-    
+$.createBtn.on('click', function(e) {
+    Ti.Platform.openURL('https://my.appcelerator.com/auth/signup');
 });
 
-$.parent.addEventListener('open', function(e) {
+$.parent.on('open', function(e) {
     $.logo.animate({ top:"28dp", duration:250 });
     
     $.header.animate({ top:"265dp", opacity:0.0, duration:250 }, function() {
